9f70ff1d052ed16894c6572e9e4373ddc97c8b46
Ayorinde Ayodeji <ayodeji780@gmail.com>
2020-05-09 13:27:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+/*
+PROJECT NAME: GEKLA PUSH NOTIFIER
+PROJECT DEVELOPER: GEEK DEELINO X OLAWALE
+
+*/
+
 // This file includes all responsive JS to make the application work better
+// Edit if only told to do so
