002d31dc4ee0b1fac605bbcc9591c539ac371a3e
Ayorinde Ayodeji <ayodeji780@gmail.com>
2020-05-09 14:15:09 +0100

    Main Gekla Notifier

---
+++ Wordpress Plugin/gekla-push-notifier.php
@@ -0,0 +1,16 @@
+<?php
+/**
+Plugin Name: Gekla Push Notifier
+Description: Gekla Push Notifier helps you notify your users when an update is made on your Wordpress Hosted Platform.
+Version: 1.0
+Author: Geek Deelino
+Author URI: https://geekdeelino.github.io
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+
+defined('ABSPATH') or die('This page may not be accessed directly.');
+define('GPN_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+
